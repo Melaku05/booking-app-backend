90097a092a81086d09b0b7e9ac6fef38f9428438
Ismail Courr <contact@ismailcourr.ml>
2022-08-22 15:42:06 +0100

    Add validations and username

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_072639) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
