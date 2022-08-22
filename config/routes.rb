9a51efb6f876cfd6497b2922310c57b15aadec79
Ismail Courr <contact@ismailcourr.ml>
2022-08-22 09:04:56 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+    get '/member-data', to: 'members#show'
   # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
