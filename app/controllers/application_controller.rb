4b31ea04d092d50e691315a371f76dc93175fc15
Ismail Courr <contact@ismailcourr.ml>
2022-08-23 09:53:45 +0100

    add authenticate before getting any data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
   protected
 
   def configure_permitted_parameters
