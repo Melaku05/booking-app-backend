f53f1aaaf0b622bd22b2bf79dde24c96ea0d0f94
melaku05 <melakushiferaw05@gmail.com>
2022-08-23 08:38:33 +0300

    Create model, controler ,and route for doctor

---
+++ app/controllers/api/v1/doctors_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::DoctorsController < ApplicationController
+  #before_action :authenticate_user!
+  before_action :set_doctor, only: %i[ show update destroy ]
+  # GET /doctors
+  def index
+    @doctors = Doctor.all
+    if @doctors.empty?
+      json_response({ msg: 'No doctor found' }, 400)
+    else
+      render json: @doctors
+    end
+    
+  end
+  # GET /doctors/1
+  def show
+    render json: @doctor
+  end
+  # POST /doctors
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      render json: @doctor, status: :created, location: @doctor
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+  # PATCH/PUT /doctors/1
+  def update
+    if @doctor.update(doctor_params)
+      render json: @doctor
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+  # DELETE /doctors/1
+  def destroy
+    @doctor.destroy
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_doctor
+      @doctor = Doctor.find(params[:id])
+    end
+    # Only allow a list of trusted parameters through.
+    def doctor_params
+      params.require(:doctor).permit(:name, :detail, :photo, :city, :specialization, :fee)
+    end
+end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,10 @@
 class Doctor < ApplicationRecord
+    has_many :users, through: :reservations
+    has_many :reservations, dependent: :destroy
+    validates :name, presence: true, length: { in: 0..50 }
+    validates :detail, presence: true, length: { in: 0..500 }
+    validates :photo, presence: true, length: { in: 0..240}
+    validates :city, presence: true, length: {in: 0..50 }
+    validates :specialization, presence: true, length: { in: 0..100 }
+    validates :fee, presence: true, numericality: { greater_than: 0 }
 end
