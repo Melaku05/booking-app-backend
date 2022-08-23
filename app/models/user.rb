7287a94ce3326d06dac76173a3bf1d0d5fc98a70
aboongm <aboongm@yahoo.com>
2022-08-23 14:18:41 +0530

    Add city and date column in reservations table and update user.rb for validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  has_many :reservations, dependent: :destroy
+  has_many :doctors, through: :reservations
+       
+  validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_072639) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_084359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_072639) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "doctor_id", null: false
+    t.string "city"
+    t.string "date"
     t.index ["doctor_id"], name: "index_reservations_on_doctor_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
