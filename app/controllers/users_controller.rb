4fd0c4b02614d5762997d02c6a1e35b1596ce89d
Ismail Courr <contact@ismailcourr.ml>
2022-08-23 08:56:37 +0100

    Add opption to GET all user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @user = current_user
+    render json: @user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
     }
     get '/member-data', to: 'members#show'
-  # resources :users
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
