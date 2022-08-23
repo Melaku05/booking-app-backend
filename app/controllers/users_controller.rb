11b18bb9af44dac31c5b688ac25e4185c27e80bd
Ismail Courr <contact@ismailcourr.ml>
2022-08-23 10:07:23 +0100

    render a message if the user is not there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
     @user = current_user
-    render json: @user
+    if current_user
+      render json: @user
+    else
+      render json: {error: "User not found"}, status: 404
+    end
   end
 end
