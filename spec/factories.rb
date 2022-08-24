86813780450d77a75ae0fbdecbead9f39e1ce249
aboongm <aboongm@yahoo.com>
2022-08-24 20:02:03 +0530

    Update factories with reservation data

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,11 @@ FactoryBot.define do
     specialization { Faker::Lorem.sentence }
     fee { Faker::Number.decimal(2) }
   end
+
+  factory Reservation do
+    city { 'Imphal' }
+    date { '24/08/2022' }
+    user_id { 1 }
+    doctor_id { 1 }
+  end
 end
